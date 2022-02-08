6d41aad0027adfd2c57410dea1acc6093dc494cc
Andrew Bass <andrewwbass@gmail.com>
2022-02-08 14:59:14 -0500

    Added files

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+console.log("hello"),console.log("hi");
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'eval-source-map',
+  mode: 'development',
+  devServer: {
+    hot: false,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization'
+    }
+  }
+});
